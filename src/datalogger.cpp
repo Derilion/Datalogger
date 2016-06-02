48a0a6f64b33edc4fe3290b424dc8e2f0a93e081
max <mbier@gmx.net>
2016-06-02 22:08:50 +0200

    added limited multitaskingsupport glib not working

--- src/datalogger.cpp
+++ src/datalogger.cpp
@@ -6,12 +6,14 @@
 // Description : Datalogger Program for the BeagleBoneBlack Rev C in C++, Ansi-style
 //============================================================================
 
+#include <glib-2.0/glib.h>
 #include <iostream>
 #include <string>
 #include <time.h>
 #include "sendMessage/sendMessage.h"
 #include "fileHandle/fileHandle.h"
 #include "udpHandle/udpHandle.h"
+
 using namespace std;
 
 int main() {
@@ -45,5 +47,47 @@ int main() {
 
 	cout << "It Works!" << endl; // prints It Works!
 	return 0;
+}
+
+void init (void){
+
+	int fd = open( "/sys/class/gpio/gpio7/value", O_RDONLY | O_NONBLOCK );
+	GIOChannel* channel = g_io_channel_unix_new( fd );
+	GIOCondition cond = GIOCondition( G_IO_PRI );
+	guint id = g_io_add_watch( channel, cond, SPI0Handle, 0 );
+
+	fd = open( "/sys/class/gpio/gpio8/value", O_RDONLY | O_NONBLOCK );
+	GIOChannel* channel = g_io_channel_unix_new( fd );
+	GIOCondition cond = GIOCondition( G_IO_PRI );
+	guint id = g_io_add_watch( channel, cond, SPI1Handle, 0 );
+
+	fd = open( "/sys/class/gpio/gpio9/value", O_RDONLY | O_NONBLOCK );
+	GIOChannel* channel = g_io_channel_unix_new( fd );
+	GIOCondition cond = GIOCondition( G_IO_PRI );
+	guint id = g_io_add_watch( channel, cond, SPI2Handle, 0 );
+
+	fd = open( "/sys/class/gpio/gpio10/value", O_RDONLY | O_NONBLOCK );
+	GIOChannel* channel = g_io_channel_unix_new( fd );
+	GIOCondition cond = GIOCondition( G_IO_PRI );
+	guint id = g_io_add_watch( channel, cond, SPI3Handle, 0 );
+
+}
+
+static gboolean SPI0Handle( GIOChannel *channel, GIOCondition condition, gpointer user_data )
+{
+    return 1;
+}
+
+static gboolean SPI1Handle( GIOChannel *channel, GIOCondition condition, gpointer user_data )
+{
+    return 1;
+}
+static gboolean SPI2Handle( GIOChannel *channel, GIOCondition condition, gpointer user_data )
+{
+    return 1;
+}
+static gboolean SPI3Handle( GIOChannel *channel, GIOCondition condition, gpointer user_data )
+{
+    return 1;
 }
 
